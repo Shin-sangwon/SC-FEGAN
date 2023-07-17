17ff5e6a9df9367a787252d15f86cb1a1e8cb5de
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-07-17 15:29:20 +0900

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,16 +1,9 @@
-import argparse
-import yaml
-import os
-import logging
-
-logger = logging.getLogger()
-
 class Config(object):
     def __init__(self, filename=None):
         assert os.path.exists(filename), "ERROR: Config File doesn't exist."
         try:
             with open(filename, 'r') as f:
-                self._cfg_dict = yaml.load(f)
+                self._cfg_dict = yaml.safe_load(f)
         # parent of IOError, OSError *and* WindowsError where available
         except EnvironmentError:
             logger.error('Please check the file with name of "%s"', filename)
