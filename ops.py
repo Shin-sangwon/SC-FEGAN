8587cb4032070771c2cd05fa0b0b9010ad0553c4
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-07-17 15:41:59 +0900

    Update ops.py

--- ops.py
+++ ops.py
@@ -3,55 +3,78 @@ import numpy as np
 import tensorflow as tf
 from tensorflow_addons.layers import InstanceNormalization
 
-# The gate convolution is made with reference to Deepfillv1(https://github.com/JiahuiYu/generative_inpainting)
+class GateConv2D(tf.keras.layers.Layer):
+    def __init__(self, cnum, ksize, stride=1, rate=1, name='conv', padding='SAME', activation='leaky_relu', use_lrn=True, training=True):
+        super(GateConv2D, self).__init__(name=name)
+        assert padding in ['SYMMETRIC', 'SAME', 'REFELECT']
+        if padding == 'SYMMETRIC' or padding == 'REFELECT':
+            p = int(rate * (ksize - 1) / 2)
+            self.padding = padding
+            self.p = p
+            padding = 'VALID'
+        else:
+            self.padding = padding
+            self.p = 0
+        self.conv = tf.keras.layers.Conv2D(
+            cnum, ksize, stride, dilation_rate=rate,
+            activation=None, padding=padding, name=name)
+        self.use_lrn = use_lrn
+        self.activation = activation
+        self.lrn = tf.keras.layers.LocallyConnected2D(1, 1)  # For local response normalization
 
-@tf.keras.utils.register_keras_serializable()
-@tf.keras.layers.Layer
-def GateConv2D(x_in, cnum, ksize, stride=1, rate=1, name='conv',
-             padding='SAME', activation='leaky_relu', use_lrn=True, training=True):
-    assert padding in ['SYMMETRIC', 'SAME', 'REFELECT']
-    if padding == 'SYMMETRIC' or padding == 'REFELECT':
-        p = int(rate * (ksize - 1) / 2)
-        x = tf.pad(x_in, [[0,0], [p, p], [p, p], [0,0]], mode=padding)
-        padding = 'VALID'
-    x = tf.keras.layers.Conv2D(
-        cnum, ksize, stride, dilation_rate=rate,
-        activation=None, padding=padding, name=name)(x_in)
-    if use_lrn:
-        x = tf.nn.local_response_normalization(x, bias=0.00005)
-    if activation == 'leaky_relu':
-        x = tf.nn.leaky_relu(x)
+    def call(self, inputs):
+        x_in = inputs
+        if self.padding == 'SYMMETRIC' or self.padding == 'REFELECT':
+            x = tf.pad(x_in, [[0, 0], [self.p, self.p], [self.p, self.p], [0, 0]], mode=self.padding)
+            padding = 'VALID'
+        else:
+            x = x_in
+            padding = self.padding
+        x = self.conv(x)
+        if self.use_lrn:
+            x = self.lrn(x)
+        if self.activation == 'leaky_relu':
+            x = tf.nn.leaky_relu(x)
 
-    g = tf.keras.layers.Conv2D(
-        cnum, ksize, stride, dilation_rate=rate,
-        activation=tf.nn.sigmoid, padding=padding, name=name + '_g')(x_in)
+        g = tf.keras.layers.Conv2D(
+            cnum, ksize, stride, dilation_rate=rate,
+            activation=tf.nn.sigmoid, padding=padding, name=name + '_g')(x_in)
 
-    x = tf.multiply(x, g)
-    return x, g
+        x = tf.multiply(x, g)
+        return x, g
 
-@tf.keras.utils.register_keras_serializable()
-@tf.keras.layers.Layer
-def GateDeconv2D(input_shape, output_shape, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-       name="deconv", training=True):
-    with tf.keras.backend.name_scope(name):
-        # filter : [height, width, output_channels, in_channels]
-        w = tf.keras.backend.random_normal([k_h, k_w, output_shape[-1], input_shape[-1]], stddev=stddev)
 
-        deconv = tf.keras.layers.Conv2DTranspose(output_shape[-1], (k_h, k_w), (d_h, d_w),
-                    padding='same', output_padding=None, strides=(d_h, d_w),
-                    kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev))(input_shape)
+class GateDeconv2D(tf.keras.layers.Layer):
+    def __init__(self, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02, name="deconv", training=True):
+        super(GateDeconv2D, self).__init__(name=name)
+        self.k_h = k_h
+        self.k_w = k_w
+        self.d_h = d_h
+        self.d_w = d_w
+        self.stddev = stddev
+        self.deconv = tf.keras.layers.Conv2DTranspose(
+            output_shape[-1], (k_h, k_w), (d_h, d_w),
+            padding='same', output_padding=None, strides=(d_h, d_w),
+            kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev))
+        self.biases = tf.Variable(tf.zeros([output_shape[-1]]))
+        self.g = tf.keras.layers.Conv2DTranspose(
+            output_shape[-1], (k_h, k_w), (d_h, d_w),
+            padding='same', output_padding=None, strides=(d_h, d_w),
+            kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev))
+        self.b = tf.Variable(tf.zeros([output_shape[-1]]))
 
-        biases = tf.keras.backend.zeros([output_shape[-1]])
-        deconv = tf.reshape(tf.keras.backend.bias_add(deconv, biases), deconv.shape)
-        deconv = tf.nn.leaky_relu(deconv)
+    def call(self, inputs):
+        input_shape, output_shape = inputs
+        with tf.keras.backend.name_scope(self.name):
+            w = tf.keras.backend.random_normal([self.k_h, self.k_w, output_shape[-1], input_shape[-1]], stddev=self.stddev)
+            deconv = self.deconv(input_shape)
+            deconv = tf.reshape(tf.keras.backend.bias_add(deconv, self.biases), deconv.shape)
+            deconv = tf.nn.leaky_relu(deconv)
 
-        g = tf.keras.layers.Conv2DTranspose(output_shape[-1], (k_h, k_w), (d_h, d_w),
-                    padding='same', output_padding=None, strides=(d_h, d_w),
-                    kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev))(input_shape)
-        b = tf.keras.backend.zeros([output_shape[-1]])
-        g = tf.reshape(tf.keras.backend.bias_add(g, b), deconv.shape)
-        g = tf.nn.sigmoid(deconv)
+            g = self.g(input_shape)
+            g = tf.reshape(tf.keras.backend.bias_add(g, self.b), deconv.shape)
+            g = tf.nn.sigmoid(deconv)
 
-        deconv = tf.multiply(g, deconv)
+            deconv = tf.multiply(g, deconv)
 
-        return deconv, g
+            return deconv, g
