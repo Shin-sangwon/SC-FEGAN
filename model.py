70fbd9e336cb69abac546059e3daa540958cceb3
Shin-sangwon <101318750+Shin-sangwon@users.noreply.github.com>
2023-07-17 15:39:14 +0900

    Update model.py

--- model.py
+++ model.py
@@ -38,49 +38,49 @@ class Model(object):
         with tf.variable_scope(name, reuse=reuse):
             # encoder
             # encoder
-x_now = x
-x1, mask1 = GateConv2D(x, cnum, 7, 2, use_lrn=False, name='gconv1_ds')
-x2, mask2 = GateConv2D(x1, 2 * cnum, 5, 2, name='gconv2_ds')
-x3, mask3 = GateConv2D(x2, 4 * cnum, 5, 2, name='gconv3_ds')
-x4, mask4 = GateConv2D(x3, 8 * cnum, 3, 2, name='gconv4_ds')
-x5, mask5 = GateConv2D(x4, 8 * cnum, 3, 2, name='gconv5_ds')
-x6, mask6 = GateConv2D(x5, 8 * cnum, 3, 2, name='gconv6_ds')
-x7, mask7 = GateConv2D(x6, 8 * cnum, 3, 2, name='gconv7_ds')
-
-# dilated conv
-x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=2, name='co_conv1_dlt')
-x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=4, name='co_conv2_dlt')
-x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=8, name='co_conv3_dlt')
-x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=16, name='co_conv4_dlt')
-
-# decoder
-x8, _ = gate_deconv(x7, [self.batch_size, s_h64, s_w64, 8 * cnum], name='deconv1')
-x8 = tf.concat([x6, x8], axis=3)
-x8, mask8 = GateConv2D(x8, 8 * cnum, 3, 1, name='gconv8')
-
-x9, _ = gate_deconv(x8, [self.batch_size, s_h32, s_w32, 8 * cnum], name='deconv2')
-x9 = tf.concat([x5, x9], axis=3)
-x9, mask9 = GateConv2D(x9, 8 * cnum, 3, 1, name='gconv9')
-
-x10, _ = gate_deconv(x9, [self.batch_size, s_h16, s_w16, 8 * cnum], name='deconv3')
-x10 = tf.concat([x4, x10], axis=3)
-x10, mask10 = GateConv2D(x10, 8 * cnum, 3, 1, name='gconv10')
-
-x11, _ = gate_deconv(x10, [self.batch_size, s_h8, s_w8, 4 * cnum], name='deconv4')
-x11 = tf.concat([x3, x11], axis=3)
-x11, mask11 = GateConv2D(x11, 4 * cnum, 3, 1, name='gconv11')
-
-x12, _ = gate_deconv(x11, [self.batch_size, s_h4, s_w4, 2 * cnum], name='deconv5')
-x12 = tf.concat([x2, x12], axis=3)
-x12, mask12 = GateConv2D(x12, 2 * cnum, 3, 1, name='gconv12')
-
-x13, _ = gate_deconv(x12, [self.batch_size, s_h2, s_w2, cnum], name='deconv6')
-x13 = tf.concat([x1, x13], axis=3)
-x13, mask13 = GateConv2D(x13, cnum, 3, 1, name='gconv13')
-
-x14, _ = gate_deconv(x13, [self.batch_size, s_h, s_w, 3], name='deconv7')
-x14 = tf.concat([x_now, x14], axis=3)
-x14, mask14 = GateConv2D(x14, 3, 3, 1, activation=None, use_lrn=False, name='gconv14')
+            x_now = x
+            x1, mask1 = GateConv2D(x, cnum, 7, 2, use_lrn=False, name='gconv1_ds')
+            x2, mask2 = GateConv2D(x1, 2 * cnum, 5, 2, name='gconv2_ds')
+            x3, mask3 = GateConv2D(x2, 4 * cnum, 5, 2, name='gconv3_ds')
+            x4, mask4 = GateConv2D(x3, 8 * cnum, 3, 2, name='gconv4_ds')
+            x5, mask5 = GateConv2D(x4, 8 * cnum, 3, 2, name='gconv5_ds')
+            x6, mask6 = GateConv2D(x5, 8 * cnum, 3, 2, name='gconv6_ds')
+            x7, mask7 = GateConv2D(x6, 8 * cnum, 3, 2, name='gconv7_ds')
+            
+            # dilated conv
+            x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=2, name='co_conv1_dlt')
+            x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=4, name='co_conv2_dlt')
+            x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=8, name='co_conv3_dlt')
+            x7, _ = GateConv2D(x7, 8 * cnum, 3, 1, rate=16, name='co_conv4_dlt')
+            
+            # decoder
+            x8, _ = gate_deconv(x7, [self.batch_size, s_h64, s_w64, 8 * cnum], name='deconv1')
+            x8 = tf.concat([x6, x8], axis=3)
+            x8, mask8 = GateConv2D(x8, 8 * cnum, 3, 1, name='gconv8')
+            
+            x9, _ = gate_deconv(x8, [self.batch_size, s_h32, s_w32, 8 * cnum], name='deconv2')
+            x9 = tf.concat([x5, x9], axis=3)
+            x9, mask9 = GateConv2D(x9, 8 * cnum, 3, 1, name='gconv9')
+            
+            x10, _ = gate_deconv(x9, [self.batch_size, s_h16, s_w16, 8 * cnum], name='deconv3')
+            x10 = tf.concat([x4, x10], axis=3)
+            x10, mask10 = GateConv2D(x10, 8 * cnum, 3, 1, name='gconv10')
+            
+            x11, _ = gate_deconv(x10, [self.batch_size, s_h8, s_w8, 4 * cnum], name='deconv4')
+            x11 = tf.concat([x3, x11], axis=3)
+            x11, mask11 = GateConv2D(x11, 4 * cnum, 3, 1, name='gconv11')
+            
+            x12, _ = gate_deconv(x11, [self.batch_size, s_h4, s_w4, 2 * cnum], name='deconv5')
+            x12 = tf.concat([x2, x12], axis=3)
+            x12, mask12 = GateConv2D(x12, 2 * cnum, 3, 1, name='gconv12')
+            
+            x13, _ = gate_deconv(x12, [self.batch_size, s_h2, s_w2, cnum], name='deconv6')
+            x13 = tf.concat([x1, x13], axis=3)
+            x13, mask13 = GateConv2D(x13, cnum, 3, 1, name='gconv13')
+            
+            x14, _ = gate_deconv(x13, [self.batch_size, s_h, s_w, 3], name='deconv7')
+            x14 = tf.concat([x_now, x14], axis=3)
+            x14, mask14 = GateConv2D(x14, 3, 3, 1, activation=None, use_lrn=False, name='gconv14')
 
 
             output = tf.tanh(x14)
